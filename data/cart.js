3038b6c94de785610eb39707f9421efc26ca1188
huydev0401 <quanghuynguyennqh@gmail.com>
2023-10-06 22:10:19 +0700

    save data to local store

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,20 @@
-export let cart = [
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [
+    {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+const saveToStore = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 export const addToCart = (productId) => {
   let matchItem;
@@ -30,6 +37,8 @@ export const addToCart = (productId) => {
       quantity,
     });
   }
+
+  saveToStore();
 };
 
 export const updateCart = () => {
@@ -38,6 +47,8 @@ export const updateCart = () => {
     cartQuantity += item.quantity;
   });
   document.querySelector(".cart-quantity").innerHTML = cartQuantity;
+
+  saveToStore();
 };
 
 export const removeFromCart = (productId) => {
@@ -48,4 +59,6 @@ export const removeFromCart = (productId) => {
     }
   });
   cart = newCart;
+
+  saveToStore();
 };
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { moneyCalculator } from "./utils/money.js";
 
-let checkoutHTML;
+let checkoutHTML = "";
 
 cart.forEach((cartItem) => {
   let matchItem;
